a04d6212232c8d3091c7f75b20d23663b2103da3
nam.dinh <test@a.com>
2017-12-27 15:53:49 +0700

    install

--- test/files/default/install.sh
+++ test/files/default/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 curl -sL https://rpm.nodesource.com/setup_6.x | bash -
 yum install -y nodejs
-# yum install gcc-c++ make -y
-# npm install -g forever
-# yum install git -y
+yum install gcc-c++ make -y
+npm install -g forever
+yum install git -y
--- test/recipes/default.rb
+++ test/recipes/default.rb
@@ -1,10 +1,27 @@
 
 Chef::Log.info("******Creating ENV.******")
 
+app = search(:aws_opsworks_app).first
+app_path = "/srv/#{app['shortname']}"
+
+Chef::Log.info("********** The app's short name is '#{app['shortname']}' **********")
+Chef::Log.info("********** The app's URL is '#{app['app_source']['url']}' **********")
+
+git app_path do
+  repository app["app_source"]["url"]
+  revision 'master'
+  action :sync
+end
+
 cookbook_file "/tmp/install_env" do
   source "install.sh"
   owner 'root'
   group 'root'
   mode 0777
-  action :create_if_missing
+  action : create
+end
+
+execute "install_env" do
+  user "root"
+  command "/tmp/install_env"
 end
